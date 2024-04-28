190b77bccb6261a81c6d22be0fe57537d9b2add8
Ext.Server Tombak <ext.server.tombak@sgs.com>
2024-04-28 08:20:34 +0300

    Add project files.

---
+++ Serilog/Middleware/RequestLogContextMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Serilog.Context;
+using System.Diagnostics;
+
+namespace Serilogg.Middleware
+{
+    public class RequestLogContextMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        public RequestLogContextMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        
+        public Task InvokeAsync(HttpContent context)
+        {
+            using(LogContext.PushProperty("CorrelationId", context.ToString()))
+            {
+                return _next(context);
+            }
+        }
+    }
+
+    
+}
